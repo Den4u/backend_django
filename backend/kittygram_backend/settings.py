c307cf7da88a92cacffc7c301c1f5384ded53fea
Deni <den4uuu@yandex.ru>
2023-11-05 08:56:17 +0000

    fst

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
